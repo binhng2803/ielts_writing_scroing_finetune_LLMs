17f590c55af1da6bdd89c6246219b8721f566c15
binhng2803 <binhng2803@gmail.com>
2024-05-22 16:54:30 +0700

    add finetune file

---
+++ finetune.py
@@ -0,0 +1,80 @@
+import os
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    TrainingArguments,
+    pipeline,
+)
+from peft import LoraConfig, PeftModel, prepare_model_for_kbit_training
+from trl import SFTTrainer
+
+hf_token = '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'
+new_model = "mistral_instruct_finetune_v1_ielts_writing_scoring"
+model_id = "mistralai/Mistral-7B-Instruct-v0.2"
+tokenizer = AutoTokenizer.from_pretrained(model_id, token = hf_token)
+
+dataset = load_dataset("binhng/scoring_ielts_dataset_v2", split="train")
+tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
+tokenizer.pad_token = tokenizer.eos_token
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+)
+
+peft_config = LoraConfig(
+    r=32,
+    lora_alpha=16,
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj"]
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    quantization_config=bnb_config,
+    device_map='auto',
+    token = hf_token,
+#     device_map='cpu'
+    # low_cpu_mem_usage=True
+)
+
+model = prepare_model_for_kbit_training(model)
+
+training_arguments = TrainingArguments(
+        output_dir="./results",
+        num_train_epochs=2,
+        per_device_train_batch_size=4,
+        gradient_accumulation_steps=4,
+        evaluation_strategy="steps",
+        eval_steps=1000,
+        logging_steps=4,
+        optim="paged_adamw_8bit",
+        learning_rate=2e-4,
+        lr_scheduler_type="linear",
+        warmup_steps=10,
+        report_to="wandb",
+        # max_steps=2, # Remove this line for a real fine-tuning
+)
+
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=dataset,
+    eval_dataset=dataset,
+    peft_config=peft_config,
+    dataset_text_field="chat",
+    max_seq_length=512,
+    tokenizer=tokenizer,
+    args=training_arguments,
+)
+
+trainer.train()
+trainer.model.save_pretrained(new_model)
+model.push_to_hub(new_model, use_temp_dir=False, token=hf_token)
+tokenizer.push_to_hub(new_model, use_temp_dir=False, token=hf_token)
